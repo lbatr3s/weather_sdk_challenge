2713e5ab70520993e1a95daf7199a84c51a0f3e3
Lester Batres <lester96@gmail.com>
2024-10-22 03:12:04 +0200

    -Added comments.

--- Weather SDK/Sources/SDK/WeatherSDK.swift
+++ Weather SDK/Sources/SDK/WeatherSDK.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 
+/// Use the delegate to listen for SDK update
 public protocol WeatherSDKDelegate: AnyObject {
     
+    /// This method is called when user taps on back button.
+    /// - Parameters:
+    ///   - controller: The ViewController to be dismissed.
     func onFinished(controller: WeatherForecastViewController)
+    
+    /// This method is called when a failure ocurrs in the SDK.
+    /// - Parameters:
+    ///   - error: The error ocurred in the SDK
+    ///   - controller: The ViewController to be dismissed.
     func onFinishedWithError(_ error: String, controller: WeatherForecastViewController)
 }
 
@@ -24,6 +33,12 @@ public struct WeatherSDK {
         storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
     }
     
+    ///  Use this method to retrieve the Weather Forecast of a given City.
+    ///
+    ///  - Parameters:
+    ///     - viewController: The viewController that will present forecast results
+    ///     - cityName: The City to retrieve the forecast
+    ///     - delegate: The delegate to listen for SDK updates.
     public func showForecastController(viewController: UIViewController, cityName: String, delegate: WeatherSDKDelegate? = nil) {
         let weatherForecastController = storyboard.instantiateViewController(withIdentifier: WeatherForecastViewController.identifier) as! WeatherForecastViewController
         
